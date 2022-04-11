3a982ec8774a786c8e78f1dee8213e280a0b9dc0
zaida-hammel <99354763+zaida-hammel@users.noreply.github.com>
2022-04-11 17:26:43 +0000

    edited code

--- sw.js
+++ sw.js
@@ -25,15 +25,6 @@ self.addEventListener("fetch", function (e) {
   )
 })
 
-self.addEventListener("install", function (e) {
-  e.waitUntil(
-    caches.open(CACHE_NAME).then(function (cache) {
-      console.log("Installing cache : " + CACHE_NAME)
-      return cache.addAll(URLS)
-    })
-  )
-})
-
 self.addEventListener("activate", function (e) {
   e.waitUntil(
     caches.keys().then(function (keyList) {
